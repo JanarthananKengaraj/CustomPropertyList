fba59de362711a2777f3a0eb0cd1c4f5e5c6d810
JanaKiOS <Janarthanankece@gmail.com>
2022-09-05 12:12:58 +0530

    Update

--- Sources/CustomPropertyList/InfoListModel.swift
+++ Sources/CustomPropertyList/InfoListModel.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 public struct InfoListModel: Codable {
-    public let env: EnvironmentInfo
+    public var env: EnvironmentInfo
     init() {
         env = EnvironmentInfo()
     }
 }
 
 public struct EnvironmentInfo: Codable {
-    public let baseUrl: String
-    public let apikey: String
-    public let build: String
+    public var baseUrl: String
+    public var apikey: String
+    public var build: String
     
     init() {
         baseUrl = ""
